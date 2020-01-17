6a70233fce233f08eaa3a2ea7ace3c3d209dbbe2
Gianluca Moro <giangiammy@gmail.com>
2020-01-17 17:08:15 +0100

    read staff via API

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,12 +4,14 @@ if [ ! -f .env.local ]; then
   exit 0;
 fi 
 
-PROJECT=`grep PROJECT .env.local | cut -d'=' -f2`
+PROJECT=`grep PROJECT_DIRECTORY .env.local | head -n 1 | cut -d'=' -f2`
 DEPLOY_TO=`grep DEPLOY_HOST .env.local | cut -d'=' -f2`
 DEPLOY_HOST=`echo $DEPLOY_TO | cut -d':' -f1`
 
+echo "Deploy $PROJECT"
+
+rm -rf var/cache/*
 cd ..
-rm -rf $PROJECT/var/cache/*
 #rsync -a --delete --exclude var/data.db --exclude public/local $PROJECT root@$DEPLOY_TO
 #rsync -a --delete --exclude public/local $PROJECT root@$DEPLOY_TO
 rsync -a --delete $PROJECT root@$DEPLOY_TO
@@ -17,7 +19,7 @@ ssh root@$DEPLOY_HOST "echo 'APP_ENV=dev' >> /var/www/html/$PROJECT/.env.local"
 ssh root@$DEPLOY_HOST "chown -R apache:apache /var/www/html/$PROJECT"
 
 #scp root@${DEPLOY_TO}/$PROJECT/var/data.db $PROJECT/var
-scp -r root@${DEPLOY_TO}/$PROJECT/public/local $PROJECT/public
+scp -r -q root@${DEPLOY_TO}/$PROJECT/public/local $PROJECT/public
 
 cd $PROJECT
 
